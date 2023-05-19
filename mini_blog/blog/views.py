7f610d52e37b9c12872e695d7240b71feabea59a
Egor <gogulaevegor@gmail.com>
2023-05-20 01:46:15 +0300

    20.05.2023 Урок - 3

--- mini_blog/blog/views.py
+++ mini_blog/blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+from .models import Post
+
+
+# Алгоритм работы с контролером:
+# 1 - создать модель; 2 - сделать миграцию; 3 - создать класс-контролер(View);
+# 4 - создать папку для хранения шаблонов проекта и под каждое приложение создать папку с шаблонами;
+# 5 - создать html-шаблон в папке с шаблонами для конкретного проекта;
+# 6 - прописать в settings.py путь к шаблонам в TEMPLATES в ключе 'DIRS';
+# 7 - в классе-контролере реализовать вывод render с прописанным путем к шаблону и контентом в формате словаря;
+# 8 - заполнить html-шаблон контентом;
+# 9 - создать в папке с приложением файл urls.py и в нем создать список urlpatterns и наполнить его по примеру, как это
+# реализовано в urls.py проекта - "urlpatterns = [path('', views.SomeView.as_view())]"
+
+class PostView(View):
+    '''Контролер постов'''
+
+    def get(self, request):
+        posts = Post.objects.all()  # запрос всех записей постов
+        return render(request=request, template_name='blog/blog.html', context={'post_list': posts})  # рендер объединяет шаблон, со словарем данных(которые можно получить из БД)
+
--- mini_blog/mini_blog/settings.py
+++ mini_blog/mini_blog/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'mini_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Прописываем путь к html-шаблонам
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
